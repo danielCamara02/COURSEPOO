c564f3c4031ecc8592b43886a3516b38b8ae8673
Daniel Camara <danielcamaradarocha08@gmail.com>
2023-05-04 11:49:53 -0300

    first commit

---
+++ App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void main(String[] args) {
+
+        //Instanciando uma classe e criando um objeto a partir dela
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.nome = "Daniel";
+        pessoa1.idade = 20;
+
+        System.out.println(pessoa1.nome);
+        System.out.println(pessoa1.idade);
+    }
+
+}
