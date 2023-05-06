06a2ad57029ed08cf7443bab39ea4b8a4dfaaf3d
Daniel Camara <danielcamaradarocha08@gmail.com>
2023-05-06 01:16:14 -0300

    Creating Getters and Setters

--- App.java
+++ App.java
@@ -4,11 +4,11 @@ public class App {
 
         //Instanciando uma classe e criando um objeto a partir dela
         Pessoa pessoa1 = new Pessoa();
-        pessoa1.nome = "Daniel";
-        pessoa1.idade = 20;
+        pessoa1.setNome("Daniel");
+        pessoa1.setIdade(20);
 
-        System.out.println(pessoa1.nome);
-        System.out.println(pessoa1.idade);
+        System.out.println(pessoa1.getNome());
+        System.out.println(pessoa1.getIdade());
     }
 
 }
--- Pessoa.java
+++ Pessoa.java
@@ -1,4 +1,31 @@
 public class Pessoa {
-    public String nome;
-    public Integer idade;
+    //Atributos
+    private String nome;
+    private Integer idade;
+
+    //Getters e Setters
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public Integer getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
 }
+
+/*
+* METODOS ACESSORES
+*
+* PUBLIC: Qualquer um pode acessar o elemento
+* PRIVATE: Somente a classe do elemento consegue acessa-lo
+* PROTECTED: Somente a classe e seus filhos conseguem acessar
+* DEFAULT: Somente a classe, seus filhos e as classes no mesmo package podem acessar
+*/
